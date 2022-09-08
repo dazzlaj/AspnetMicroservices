2b50cd2196c416693519e9e427fce19257645573
dazzlaj <Darren.James@VERTICALSYSTEMS.LIMITED>
2022-09-08 21:48:01 +0100

    First Run

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>("DatbaseSettings:ConnectionString"));
+            var database = client.GetDatabase(config.GetValue<string>("DatbaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(config.GetValue<string>("DatbaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+                
+        } 
+
+        public IMongoCollection<Product> Products { get; }
+
+
+    }
+}
